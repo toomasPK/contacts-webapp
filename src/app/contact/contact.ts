2ac0521fb6d0432085d518a2eee8374402676c37
toomasPK <toomas.karvinen@gmail.com>
2018-05-16 12:10:42 +0300

    Added contact service, synchronous implementation

--- src/app/contact/contact.ts
+++ src/app/contact/contact.ts
@@ -1,9 +1,11 @@
 export class Contact {
+  id: number;
   firstName: string;
   lastName: string;
 
 
-  constructor(firstName?: string, lastName?: string) {
+  constructor (id?: number, firstName?: string, lastName?: string) {
+    this.id = id;
     this.firstName = firstName;
     this.lastName = lastName;
   }
