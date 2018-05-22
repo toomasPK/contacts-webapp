605b6326058d641ef53a56f01a8c5b870fffd325
toomasPK <toomas.karvinen@gmail.com>
2018-05-22 14:12:02 +0300

    Refactored contact detail component to enable operating with each other

--- src/app/contact/contact-detail/contact-detail.component.ts
+++ src/app/contact/contact-detail/contact-detail.component.ts
@@ -14,32 +14,38 @@ import {ToolbarAction} from '../../ui/toolbar/toolbar-action';
 export class ContactDetailComponent implements OnInit {
 
   contact: Contact;
+  editingEnabled: boolean;
 
   constructor(private router: Router, private route: ActivatedRoute, private contactService: ContactService,
               private toolbar: ToolbarService) {
     this.contact = new Contact();
+    this.editingEnabled = false;
   }
 
   ngOnInit() {
-    this.toolbar.toolbarOptions.next(new ToolbarOptions('contact',[
-      new ToolbarAction(this.onEdit, 'edit')
-    ]));
+
 
     const contactId = this.route.snapshot.paramMap.get('id');
-    console.log(contactId);
+    let toolbarAction: ToolbarAction[];
 
     if(contactId == null) {
-      return
+      // Create contact
+      this.editingEnabled = true;
+      toolbarAction = [];
+    } else {
+      // View/Edit contact
+      toolbarAction = [new ToolbarAction(this.onEdit.bind(this), 'edit' )]
+
+      this.contactService.getContactById(contactId).subscribe(response => {
+        this.contact = response;
+        console.log(this.contact);
+      }, error => {
+        console.error('Getting contact failed');
+        console.error(error);
+        this.router.navigate (['/contacts']);
+      });
     }
 
-    this.contactService.getContactById(contactId).subscribe(response => {
-      this.contact = response;
-      console.log(this.contact);
-    }, error => {
-      console.error('Getting contact failed');
-      console.error(error);
-      this.router.navigate (['/contacts']);
-    });
   }
 
   onNavigateBack(): void {
@@ -51,7 +57,7 @@ export class ContactDetailComponent implements OnInit {
   }
 
   onEdit(): void {
-    console.log('No wörk')
+    this.editingEnabled = !this.editingEnabled;
   }
 
 }
